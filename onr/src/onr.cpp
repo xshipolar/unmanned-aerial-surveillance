95bef0a8f951fc220c4601d4fe9992e4455e961d
uav-dev <uiucuav@gmail.com>
2012-11-25 23:32:09 -0600

    onr: implemented threading

--- onr/src/onr.cpp
+++ onr/src/onr.cpp
@@ -3,12 +3,9 @@
  *
  * Created on : 07-01-2012
  *     Author : Xichen Shi
- *       Rev  : 10-22-2012
+ *       Rev  : 11-25-2012
  */
 
-//#include <boost/asio.hpp>
-//#include <boost/bind.hpp>
-//#include <boost/date_time/posix_time/posix_time.hpp>
 #include <iostream>
 #include <inttypes.h>
 #include <ctime>
@@ -22,18 +19,34 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#include "../include/mavlink/mavlink_types.h"
-#include "../include/mavlink/ardupilotmega/mavlink.h"
+#include <mavlink/mavlink_types.h>
+#include <mavlink/ardupilotmega/mavlink.h>
 
 using namespace std;
 using namespace cv;
 
-/** Variables **/
-const char *serialName = "/dev/ttyS0";
-const char *window1 = "Video";
+/////////////////////////////////////////////////////////////////////
+///////////////////////// System Status /////////////////////////////
+/////////////////////////////////////////////////////////////////////
+bool alive = true;
 uint8_t buf[MAVLINK_MAX_PACKET_LEN];
 
-/** Functions **/
+/////////////////////////////////////////////////////////////////////
+////////////////////////// Video Src ////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+VideoCapture videoSrc0(0);
+const char *window1 = "Video";
+   
+/////////////////////////////////////////////////////////////////////
+//////////////////////// Serial Port ////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+const char *serialName = "/dev/ttyS0";
+termios serialConfig;
+int Serial;
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////// Functions ////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 void setSerialPort(termios *serialIO, int& tid)
 {
     serialIO->c_iflag = 0;
@@ -49,29 +62,32 @@ void setSerialPort(termios *serialIO, int& tid)
     tcsetattr(tid, TCSANOW, serialIO);
 }
 
-void captureVideo(VideoCapture vidsrc, const char *windowName)
+void captureVideo()
 {
     Mat frame;
-    vidsrc>>frame;
-    imshow(windowName, frame);
+    videoSrc0>>frame;
+    imshow(window1, frame);
 }
 
-void logData(FILE *fid, int port)
+void handleMessage()
 {
     static unsigned char c;
     mavlink_message_t msg;
     mavlink_status_t status;
-    if(read(port, &c, 1)>0) {
+    if(read(Serial, &c, 1)>0) {
         if (mavlink_parse_char(0, c, &msg, &status)){
             switch (msg.msgid) {
             case MAVLINK_MSG_ID_RAW_IMU:
                 mavlink_raw_imu_t raw_imu;
                 mavlink_msg_raw_imu_decode(&msg, &raw_imu);
-	            fprintf(fid, "%12d,%10d,%10d,%10d,%10d,%10d,%10d\n",raw_imu.time_usec,raw_imu.xacc,raw_imu.yacc,raw_imu.zacc,raw_imu.xgyro,raw_imu.ygyro,raw_imu.zgyro);
-		        printf("%12d,%10d,%10d,%10d,%10d,%10d,%10d\n",raw_imu.time_usec,raw_imu.xacc,raw_imu.yacc,raw_imu.zacc,raw_imu.xgyro,raw_imu.ygyro,raw_imu.zgyro);
+	            //fprintf(fid, "%12d,%10d,%10d,%10d,%10d,%10d,%10d\n",raw_imu.time_usec,raw_imu.xacc,raw_imu.yacc,raw_imu.zacc,raw_imu.xgyro,raw_imu.ygyro,raw_imu.zgyro);
+		        //printf("%12d,%10d,%10d,%10d,%10d,%10d,%10d\n",raw_imu.time_usec,raw_imu.xacc,raw_imu.yacc,raw_imu.zacc,raw_imu.xgyro,raw_imu.ygyro,raw_imu.zgyro);
 		        break;		
 		        
-	        case MAVLINK_MSG_ID_ATTITUDE:
+	        case MAVLINK_MSG_ID_GLOBAL_POSITION_INT:
+	            mavlink_global_position_int_t gps;
+	            mavlink_msg_global_position_int_decode(&msg, &gps);
+	            printf("%10d,%10d,%10d,%10d,%10d,%10d,%10d,%10d,%10d\n",gps.time_boot_ms, gps.lat, gps.lon, gps.lat, gps.relative_alt, gps.vx, gps.vy, gps.vz, gps.hdg);
 	            break;
 	           
             case MAVLINK_MSG_ID_HEARTBEAT:
@@ -87,18 +103,33 @@ void logData(FILE *fid, int port)
     }
 }
 
+void *runThread1(void*)
+{
+    while(alive)
+    {
+        captureVideo();
+        char c = waitKey(33);
+        if (c==27) alive=false;
+    }
+}
+
+void *runThread2(void*)
+{
+    while(alive)
+    {
+        handleMessage();
+    }
+}
+      
 int main()
 {   
     // Serial through termios
-    termios serialConfig;
     memset(&serialConfig, 0, sizeof(serialConfig));
-    int Serial;
-    
     setSerialPort(&serialConfig, Serial);
     
     // Video Capture
-    VideoCapture capture(0);
-    cvNamedWindow(window1,CV_WINDOW_AUTOSIZE);
+    
+    namedWindow(window1,CV_WINDOW_AUTOSIZE);
     
     // Check port status
     if (Serial != -1) {
@@ -110,34 +141,25 @@ int main()
     }
     
     // Check capture device status
-    if (!capture.isOpened()) {
+    if (!videoSrc0.isOpened()) {
 	    cout << "capture device failed to open!" << endl;
-        return 1;
+        return -1;
     }
 
     // serial_port reading and writing
     int count = 0;
-    FILE *log;
-    log = fopen("testdata", "w");
-    fprintf(log, "     time(us)   xacc(mg)   yacc(mg)   zacc(mg)      xgyro      ygyro      zgyro\n");
+    //FILE *log;
+    //log = fopen("testdata", "w");
+    //fprintf(log, "     time(us)   xacc(mg)   yacc(mg)   zacc(mg)      xgyro      ygyro      zgyro\n");
     printf("     time(us)   xacc(mg)   yacc(mg)   zacc(mg)      xgyro      ygyro      zgyro\n");
     
     // Mainloop
-    for(;;)
-    {           
-        // Capture image
-        //captureVideo(capture, window1);
-        /*if(cvWaitKey(10) == 'q')
-        {
-            fclose(log);
-            return 0;
-        }*/
-        
-        // Comm
-        logData(log, Serial);
-        
-	    /** increase main loop count **/
-	    count++;
-    }
+    pthread_t thread1, thread2;
+
+	pthread_create( &thread1, NULL, runThread1, NULL);
+	pthread_create( &thread2, NULL, runThread2, NULL);
+    pthread_join( thread1, NULL );
+    pthread_join( thread2, NULL );
+
     return 0;
 }
