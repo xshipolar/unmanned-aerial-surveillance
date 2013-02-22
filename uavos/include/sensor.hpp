0d2ffc777ee2cc61addd831a6c01e4bd97f0a06b
Xichen Shi <sxc901231@gmail.com>
2013-02-22 10:48:27 -0600

    UAS: sensor functions for holding and updating data

---
+++ uavos/include/sensor.hpp
@@ -0,0 +1,40 @@
+/*
+ * sensor.hpp
+ *
+ *  Created on: Feb 21, 2013
+ *      Author: Xichen Shi
+ */
+
+#ifndef SENSOR_HPP_
+#define SENSOR_HPP_
+
+#include <inttypes.h>
+
+struct global_position_t {
+    int32_t lat; ///< Latitude, expressed as * 1E7
+    int32_t lon; ///< Longitude, expressed as * 1E7
+    int32_t alt; ///< Altitude in meters, expressed as * 1000 (millimeters), above MSL
+    int32_t relative_alt; ///< Altitude above ground in meters, expressed as * 1000 (millimeters)
+    int16_t vx; ///< Ground X Speed (Latitude), expressed as m/s * 100
+    int16_t vy; ///< Ground Y Speed (Longitude), expressed as m/s * 100
+    int16_t vz; ///< Ground Z Speed (Altitude), expressed as m/s * 100
+    uint16_t hdg; ///< Compass heading in degrees * 100, 0.0..359.99 degrees. If unknown, set to: 65535
+};
+
+struct attitude_t {
+    float roll; ///< Roll angle (rad)
+    float pitch; ///< Pitch angle (rad)
+    float yaw; ///< Yaw angle (rad)
+    float rollspeed; ///< Roll angular speed (rad/s)
+    float pitchspeed; ///< Pitch angular speed (rad/s)
+    float yawspeed; ///< Yaw angular speed (rad/s)
+};
+
+extern attitude_t        g_attitude;
+extern global_position_t g_global_position;
+
+void updateGlobalPosition(int32_t lat, int32_t lon, int32_t alt, int32_t relative_alt, int16_t vx, int16_t vy, int16_t vz, uint16_t hdg);
+
+void updateAttitude(float roll, float pitch, float yaw, float rollspeed, float pitchspeed, float yawspeed);
+
+#endif /* SENSOR_HPP_ */
---
+++ uavos/src/sensor.cpp
@@ -0,0 +1,31 @@
+/*
+ * sensor.cpp
+ *
+ *  Created on: Feb 21, 2013
+ *      Author: Xichen Shi
+ */
+
+#include <sensor.hpp>
+
+global_position_t g_global_position;
+attitude_t        g_attitude;
+
+void updateGlobalPosition(int32_t lat, int32_t lon, int32_t alt, int32_t relative_alt, int16_t vx, int16_t vy, int16_t vz, uint16_t hdg){
+    g_global_position.lat = lat;
+    g_global_position.lon = lon;
+    g_global_position.alt = alt;
+    g_global_position.relative_alt = relative_alt;
+    g_global_position.vx  = vx;
+    g_global_position.vy  = vy;
+    g_global_position.vz  = vz;
+    g_global_position.hdg = hdg;
+}
+
+void updateAttitude(float roll, float pitch, float yaw, float rollspeed, float pitchspeed, float yawspeed){
+    g_attitude.roll  = roll;
+    g_attitude.pitch = pitch;
+    g_attitude.yaw   = yaw;
+    g_attitude.rollspeed  = rollspeed;
+    g_attitude.pitchspeed = pitchspeed;
+    g_attitude.yawspeed   = yawspeed;
+}
